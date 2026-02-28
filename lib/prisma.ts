a76b8dc4751fbdc70a56fcdc6722d7b696720382
Vatcharamai <yoyo.rodring@gmail.com>
2026-02-28 22:52:55 +0700

    feat: Integrate Prisma Neon adapter for database connection and update Prisma dependencies.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,18 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import type { PoolConfig } from "@neondatabase/serverless";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+function createPrismaClient() {
+  const connectionString = process.env.DATABASE_URL!;
+  const config: PoolConfig = { connectionString };
+  const adapter = new PrismaNeon(config);
+  return new PrismaClient({ adapter });
+}
+
+export const prisma = globalForPrisma.prisma ?? createPrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
